8897dd54d0ab4017e2d8d42a04e98733f55e2f83
Tomoyuki KAMIYA <kamiya7140@gmail.com>
2020-04-07 09:54:18 +0900

    WIP (add validation)

--- question/app.py
+++ question/app.py
@@ -2,6 +2,9 @@ import json
 import os
 from urllib.parse import parse_qs
 import boto3
+import time
+import hmac
+import hashlib
 from datetime import datetime
 
 ddb = boto3.resource("dynamodb")
@@ -11,13 +14,44 @@ def lambda_handler(event, context):
     method = event["httpMethod"]
 
     if method == "GET":
-        result = table.scan()
-        return result
+        # For debug purpose
+        result = table.scan()["Items"]
+        return json.dumps(result)
 
     if method == "POST":
-        body = parse_qs(event["body"])
+        timestamp = event["headers"]['X-Slack-Request-Timestamp']
+
+        # Validate request
+        ## Timestamp
+        if abs(time.time() - int(timestamp)) > 60 * 5:
+            return {
+                    "statusCode": 200,
+                    "headers": {},
+                    "body": json.dumps({
+                        "response_type": "in_channel",
+                        "text": "Could not validate the integrity of request"
+                        })
+                    }
+
+        ## Signature
+        sig_basestring = 'v0:' + timestamp + ':' + event["body"]
+        my_signature = 'v0=' + hmac.new(os.getenv("SLACK_SIGNING_SECRET").encode("utf-8"), sig_basestring.encode("utf-8"), hashlib.sha256).hexdigest()
+
+        if event["headers"]['X-Slack-Signature'] != my_signature:
+            return {
+                    "statusCode": 200,
+                    "headers": {},
+                    "body": json.dumps({
+                        "response_type": "in_channel",
+                        "text": "Could not validate the integrity of request"
+                        })
+                    }
+
+        # Here, request integrity has been validated
 
         # Extract necessary fields
+        body = parse_qs(event["body"])
+        
         token = body["token"][0]
         trigger_id = body["trigger_id"][0]
         team_id = body["team_id"][0]
@@ -27,8 +61,8 @@ def lambda_handler(event, context):
 
         posted_at = datetime.now().isoformat()
 
-        # TODO: Validate request by token
-
+        
+        
         # Put item into DynamoDB
         table.put_item(Item={
             "trigger_id": trigger_id,
@@ -39,13 +73,12 @@ def lambda_handler(event, context):
             "posted_at": posted_at
         })
 
-        response_body = {
-                "response_type": "in_channel",
-                "text": "hello world"
+        return {
+                "statusCode": 200,
+                "headers": {},
+                "body": json.dumps({
+                    "response_type": "in_channel",
+                    "text": "Your question has been accepted: " + str(event)
+                    })
                 }
 
-        return {
-            "statusCode": 200,
-            "headers": {},
-            "body": json.dumps(response_body)
-        }
